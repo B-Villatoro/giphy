92a5d6787bfb0caced591cfe8dbb2b5eb28aaa88
Bryan Villatoro <bvillatoro@ymail.com>
2019-03-22 09:45:52 -0400

    starting template

---
+++ main.js
@@ -0,0 +1,3 @@
+let search;
+let giphyUrl = "https://api.giphy.com/v1/gifs/search?api_key=jZZO00sEpmWON3m5hRCUOUQPf596kQ21&q="+search+"&limit=10&offset=0&rating=PG-13&lang=en"
+let searchArray = ["taco","rubber","ducky","spongebob"]
